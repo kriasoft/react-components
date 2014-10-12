bcb056faec980466c44d9fef1bcc6b60d79ba89b
Konstantin Tarkus <hello@tarkus.me>
2014-10-12 23:52:08 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @jsx React.DOM
+ */
+
+var react = require('react');
+
+var Input = React.createClass({
+  render() {
+    return (
+      <div className="input">
+        <input type="text" />
+      </div>
+    );
+  }
+});
